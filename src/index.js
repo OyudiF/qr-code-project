8cfc75559059be091e80d0aeac6f0b9f64fe80df
Oyudi22 <ochiaifelipe@gmail.com>
2025-04-14 11:15:16 -0300

    Added password generator

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import prompt from 'prompt';
 
 import mainPrompt from "./prompts/promptMain.js";
 import createQRCode from './services/qr-code/create.js';
+import createPassword from './services/password/create.js';
 
 async function main() {
     prompt.get(mainPrompt, async (err, choice) => {
         if(choice.select == 1) await createQRCode();
-        if(choice.select == 2) console.log("Escolheu o Password generator");
-        
+        if(choice.select == 2) await createPassword();
     });
 }
 
